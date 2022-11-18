8c8f5f70f973b633694d77ca406d6ffa346eecdb
Helton Brito <heltoncarlospa@hotmail.com>
2022-11-17 21:27:13 -0300

    feat: figure and button

---
+++ src/components/Button/Button.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+defineProps<{ title: string }>()
+defineEmits<{ onClick: void }>()
+</script>
+
+<template>
+  <button class="bg-green" @click="onClick">
+   {{ title }}
+  </button>
+</template>
