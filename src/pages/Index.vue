ef7d539b6765077274fc754d04d656e307d94fb8
Helton Brito <heltoncarlospa@hotmail.com>
2022-11-17 21:06:17 -0300

    feat: page index mobile

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,7 +1,22 @@
 <script setup lang="ts"></script>
 
 <template>
-  <div>
-    <img src="../assets/Jato.svg" alt="Jato" />
+  <div class="h-screen lg:items-center lg:flex lg:justify-center">
+    <div class="m-4 mt-10 lg:mt-0">
+      <img src="../assets/Jato.svg" alt="Jato" />
+    </div>
+    <div class="mx-4 lg:w-[450px]">
+      <h1 class="w-[145px] text-white text-3xl border-b-4 border-green">
+        Aliens Free
+      </h1>
+      <h2 class="text-green lg:text-4xl pt-2 font-semibold">
+        Join the world's largest crypto exchange.
+      </h2>
+      <p class="text-gray pt-2 text-sm">
+        Lorem ipsum dolor sit amet, consectetur adipisci elit, sed do eiusmod
+        tempor incididunt ut labore et dolore magna aliqua. Senectus et netus et
+        malesuada fames.
+      </p>
+    </div>
   </div>
 </template>
