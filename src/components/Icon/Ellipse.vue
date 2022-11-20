f708c1b0887dac0aa79daf8f31e7e5fe1508bb3e
Helton Brito <heltoncarlospa@hotmail.com>
2022-11-20 20:39:24 -0300

    feat: adding ellipse

---
+++ src/components/Icon/Ellipse.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+defineProps<{ value: boolean }>()
+defineEmits<{ (e:'onClick'): void }>();
+</script>
+
+<template>
+  <div>
+    <img src="../../assets/Ellipse-on.svg" class="hover:cursor-pointer" alt="Ellipse-on" @click="$emit('onClick')" v-if="value === false">
+    <img src="../../assets/Ellipse-off.svg" alt="Ellipse-off" @click="$emit('onClick')" v-else>
+  </div>
+</template>
--- src/components/Onboarding/Onboarding.vue
+++ src/components/Onboarding/Onboarding.vue
@@ -2,18 +2,29 @@
 import { boarding } from './onBoarding';
 import { ref } from 'vue'
 import Button from "../Button/Button.vue";
+import Ellipse from '../Icon/Ellipse.vue';
 
 const board = boarding()
 const count = ref<number>(0)
+
+function allCount(index: number) {
+  count.value = index 
+}
 </script>
 
 <template>
   <div class="text-center w-5/6">
-    <img :src="board[count].image" :alt="board[count].title" class="m-auto" />
-    <h3 class="text-white pt-2 font-medium">{{ board[count].title }}</h3>
+    <img :src="board[count].image" :alt="board[count].title" class="m-auto h-[55%] mt-4" />
+    <h3 class="text-white pt-4 font-medium">{{ board[count].title }}</h3>
     <p class="text-gray pt-2 text-sm">
       {{ board[count].subtitle }}
     </p>
+    
+    <div class="flex justify-center gap-2 py-2">
+      <Ellipse :value='count === 0' @onClick='allCount(0)'/>
+      <Ellipse :value='count === 1' @onClick='allCount(1)'/> 
+      <Ellipse :value='count === 2' @onClick='allCount(2)'/>
+    </div>
 
     <Button title="Next" class="m-4" @onClick='count++' />
   </div>
