701cbffce3551c6327c9deeea62aba15073d14fa
Helton Brito <heltoncarlospa@hotmail.com>
2022-12-01 21:34:22 -0300

    feat: adding navbar

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,26 +1,12 @@
 <script setup lang="ts">
 import Card from "../components/Card/Card.vue";
+import Nav from "../components/Nav/Nav.vue";
 </script>
 
 <template>
   <div>
     <div class="w-11/12 flex justify-between mx-auto py-4">
-      <div>
-        <img
-          class="h-10 w-10 rounded-full"
-          src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
-          alt=""
-        />
-      </div>
-      <div class="flex items-center">
-        <img src="../assets/icon/Lupa.svg" alt="Lupa" class="cursor-pointer" />
-        <img src="../assets/icon/Menu.svg" alt="Menu" class="cursor-pointer" />
-        <img
-          src="../assets/icon/Notificacao.svg"
-          alt="Notificação"
-          class="cursor-pointer"
-        />
-      </div>
+      <Nav />
     </div>
 
     <div class="bg-white w-full min-h-screen py-4">
