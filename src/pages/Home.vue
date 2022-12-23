9940e1f181cf59b2428489883d2dff2d7f874d6d
Helton Brito <heltoncarlospa@hotmail.com>
2022-12-22 22:09:55 -0300

    fix: adjust page home

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -21,39 +21,33 @@ getMoney();
 </script>
 
 <template>
-  <div>
-    <div class="bg-gray-light w-full min-h-screen py-4">
-      <div class="w-11/12 mx-auto">
-        <div>
-          <h3 class="text-lg font-semibold">Services</h3>
-          <div class="md:flex md:gap-4">
-            <Card
-              imagem="./src/assets/icon/Nave.svg"
-              title="P2P Trading"
-              subtitle="Bank Transfer, Paypal Revolut..."
-              :arrow="true"
-            />
-            <Card
-              imagem="./src/assets/icon/Card.svg"
-              title="P2P Trading"
-              subtitle="Bank Transfer, Paypal Revolut..."
-              :arrow="true"
-            />
-          </div>
-        </div>
-        <div>
-          <h3 class="text-lg font-semibold">Recent Coin</h3>
-          <div class="md:flex md:gap-4">
-            <Card
-              v-for="(moeda, index) in money"
-              :key="index"
-              :imagem="moeda.image"
-              :title="maskMoney(moeda.high)"
-              :subtitle="`${moeda.code}/${moeda.codein}`"
-              :arrow="true"
-            />
-          </div>
-        </div>
+  <div class="bg-gray-light w-full min-h-screen py-4">
+    <div class="w-11/12 mx-auto">
+      <h3 class="text-lg font-semibold">Services</h3>
+      <div class="md:flex md:gap-4">
+        <Card
+          imagem="./src/assets/icon/Nave.svg"
+          title="P2P Trading"
+          subtitle="Bank Transfer, Paypal Revolut..."
+          :arrow="true"
+        />
+        <Card
+          imagem="./src/assets/icon/Card.svg"
+          title="P2P Trading"
+          subtitle="Bank Transfer, Paypal Revolut..."
+          :arrow="true"
+        />
+      </div>
+      <h3 class="text-lg font-semibold">Recent Coin</h3>
+      <div class="md:flex md:gap-4">
+        <Card
+          v-for="(moeda, index) in money"
+          :key="index"
+          :imagem="moeda.image"
+          :title="maskMoney(moeda.high)"
+          :subtitle="`${moeda.code}/${moeda.codein}`"
+          :arrow="true"
+        />
       </div>
     </div>
   </div>
