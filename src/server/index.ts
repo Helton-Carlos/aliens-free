a5e4f1b9e797198c2d70472ebde0a11dd6bf2b60
Helton Brito <heltoncarlospa@hotmail.com>
2023-01-08 00:33:02 -0300

    fix: feat client

--- src/server/index.ts
+++ src/server/index.ts
@@ -21,8 +21,8 @@ export function makeServer({ environment = "development" } = {}) {
             user: "Helton Brito",
             age: "29/07/1990",
             money: "200.00",
-            image: "https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80",
-
+            image:
+              "https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80",
           },
         ],
       }));
