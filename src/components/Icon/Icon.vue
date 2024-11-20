57697f50732027994756a3123f8ee79b687b15b4
Helton Brito <heltoncarlospa@hotmail.com>
2024-11-20 11:55:51 -0300

    feat: add dynamic icons

--- src/components/Icon/Icon.vue
+++ src/components/Icon/Icon.vue
@@ -1,14 +1,21 @@
 <script setup lang="ts">
 import Spinner from './Spinner.vue';
+import Ellipse from './Ellipse.vue';
+import Close from './Close.vue';
 
-defineProps<{ 
-  name: string, 
-  required: true 
+const icons = {
+  Spinner,
+  Ellipse,
+  Close,
+};
+
+defineProps<{
+  name: keyof typeof icons;
 }>();
 </script>
 
 <template>
   <div>
-    <component :is="Spinner"/>
+    <component :is="icons[name]" />
   </div>
 </template>
